26d95a8769123148adcd7cf1484a54af83ed80a8
Joshua Karstendick <joshdick@gmail.com>
2014-09-26 14:32:31 -0400

    added slamhound alias to lein user profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,2 +1,3 @@
 {:user {:plugins [[lein-pprint "1.1.1"]]
-        :dependencies [[slamhound "1.3.1"]]}}
+        :dependencies [[slamhound "1.3.1"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
