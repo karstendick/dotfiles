d3d3dff97e22238fe385d25f06fadac49081638c
Joshua Karstendick <joshdick@gmail.com>
2014-11-05 09:23:45 -0500

    added lein-kibit to user profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,4 @@
-{:user {:plugins [[lein-pprint "1.1.1"]]
+{:user {:plugins [[lein-pprint "1.1.1"]
+                  [lein-kibit "0.0.8"]]
         :dependencies [[slamhound "1.3.1"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
