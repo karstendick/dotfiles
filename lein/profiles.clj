f8cce4a3547f8e9dbc19c8149ff28040501edb79
Joshua Karstendick <joshdick@gmail.com>
2014-12-01 13:47:04 -0500

    added lein eastwood

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-pprint "1.1.1"]
-                  [lein-kibit "0.0.8"]]
+                  [lein-kibit "0.0.8"]
+                  [jonase/eastwood "0.2.0"]]
         :dependencies [[slamhound "1.3.1"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
