c3486dd8a904e89a7ffd2534cb8ee90e1123ba72
Joshua Karstendick <joshdick@gmail.com>
2014-12-17 14:13:35 -0500

    add midje to lein profiles.clj

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[lein-pprint "1.1.1"]
                   [lein-kibit "0.0.8"]
                   [jonase/eastwood "0.2.0"]]
-        :dependencies [[slamhound "1.3.1"]]
+        :dependencies [[slamhound "1.3.1"]
+                       [midje "1.5.0"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
