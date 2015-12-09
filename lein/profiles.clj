4aabb426ba5b5a4e1e22c933b7cadedb3960c23b
Joshua Karstendick <joshdick@gmail.com>
2015-12-09 09:37:27 -0500

    bump midje version

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,5 +2,5 @@
                   [lein-kibit "0.0.8"]
                   [jonase/eastwood "0.2.0"]]
         :dependencies [[slamhound "1.3.1"]
-                       [midje "1.5.0"]]
+                       [midje "1.7.0"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
