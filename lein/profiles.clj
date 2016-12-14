3e0aca601970e4e01a27e7d075e1dc298f0d9ff7
Joshua Karstendick <jkarstendick@rjmetrics.com>
2016-12-14 15:06:18 -0500

    add humane-test-output to lein profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,5 +2,8 @@
                   [lein-kibit "0.0.8"]
                   [jonase/eastwood "0.2.0"]]
         :dependencies [[slamhound "1.3.1"]
-                       [midje "1.7.0"]]
+                       [midje "1.7.0"]
+                       [pjstadig/humane-test-output "0.8.1"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
