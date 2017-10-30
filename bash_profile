e16d0c46999ec2bd770657a940f2948eb0ebee20
Joshua Karstendick <joshdick@gmail.com>
2017-10-29 21:28:09 -0400

    Cleanup unused stuff (#3)
    * Remove more RJMetrics stuff and work vs default configs
    
    * default ntmux to ~
    
    * more stitch cleanup
    
    * remove unused aliases
    
    * update email
    
    * removing more stitch stuff
    
    * removing more unused stuff
    
    * formatting
    
    * gitignore emacs temp files

--- bash_profile
+++ bash_profile
@@ -27,23 +27,10 @@ export EMAIL=joshdick@gmail.com
 ## Add `~/bin` to the `$PATH`
 export PATH="$HOME/bin:$PATH";
 
-source ~/aws_creds.sh
-
 # Load the shell dotfiles, and then some:
 for file in ~/.{bash_prompt,bash_aliases}; do
     [ -r "$file" ] && [ -f "$file" ] && source "$file";
 done;
-# Load the stitchdata/ide goodies:
-for file in ~/git/ide/bash/functions/*; do
-    [ -r "$file" ] && [ -f "$file" ] && source "$file";
-done;
-for file in ~/git/ide/bash/aliases/*; do
-    [ -r "$file" ] && [ -f "$file" ] && source "$file";
-done;
-for file in ~/git/ide/bash/bin/*; do
-    ln -s "$file" ~/bin;
-done;
-ln -s ~/git/ide/bash/tmux.conf ~/tmux.conf
 unset file;
 
 source ~/bin/tmux.bash
--- bin/tmux.bash
+++ bin/tmux.bash
@@ -9,12 +9,10 @@ function new_tmux_session {
         return 1
     fi
 
-    local base_dir=$2
-
     local default_command="which reattach-to-user-namespace > /dev/null 2>&1 && reattach-to-user-namespace -l bash || bash"
 
     (
-        cd $base_dir
+        cd $HOME
 
         # tmux -vvvv new-session -d -s $session_name -n editor "$default_command" # for debugging
         tmux new-session -d -s $session_name -n editor "$default_command"
@@ -36,75 +34,12 @@ function new_tmux_session {
     tmux attach -t $session_name
 }
 
-function matching_git_project() {
-    local session_name="$1"
-    local unnamespaced="${session_name##*/}"
-
-    gps=("$HOME"/!(Library)/{,*,*/*,*/*/*}/.git)
-
-    # FIXME, this and the below should be the same
-    for gp in "${gps[@]}"
-    do
-        project_name="${gp%/.git}"
-        project_name="${project_name##*/}"
-        if [[ $project_name = "$unnamespaced"* ]]
-        then
-            return 0
-        fi
-    done
-
-    return 1
-}
-
-function attach_to_git_project() {
-    local session_name="$1"
-    local ns="${session_name%/*}"
-    local unnamespaced="${session_name##*/}"
-
-    gps=("$HOME"/!(Library)/{,*,*/*,*/*/*}/.git)
-
-    # FIXME, this and the above should be the same
-    for gp in "${gps[@]}"
-    do
-        project_directory="${gp%/.git}"
-        project_name="${project_directory##*/}"
-        if [[ $project_name = "$unnamespaced"* ]]
-        then
-            new_tmux_session "$ns/$project_name" "$project_directory"
-            return 0
-        fi
-    done
-
-    echo "Couldn't find a matching git project for $session_name ($unnamespaced)" >&2
-    return 1
-
-}
-
-function matching_in_current_dir() {
-    local session_name="$1"
-
-    shopt -s nullglob
-
-    for match in "$session_name"*
-    do
-        if [[ -d $match ]]
-        then
-            echo "$match"
-            shopt -u nullglob
-            return
-        fi
-    done
-    shopt -u nullglob
-    return 1
-}
-
 function ntmux {
     local session_name="$1"
-    local base_dir="$2"
 
     if [[ -z $session_name ]]
     then
-        echo 'Usage: ntmux [namespace/]session_name [base_dir]'
+        echo 'Usage: ntmux session_name'
         return 1
     fi
 
@@ -114,20 +49,8 @@ function ntmux {
     then
         # Attach to Existing Session
         tmux attach -t "$session_name"
-    elif [[ -n $base_dir ]]
-    then
-        new_tmux_session "$session_name" "$base_dir"
-    elif matching_in_current_dir "$session_name" > /dev/null
-    then
-        local session_and_dir_name="$(matching_in_current_dir "$session_name")"
-        new_tmux_session "$session_and_dir_name"  "$session_and_dir_name"
-    elif matching_git_project "$session_name"
-    then
-        # Create a session for a git project
-        attach_to_git_project "$session_name"
     else
-        echo "Could not find existing session or git project for '$session_name' and you specified no base directory." >&2
-        return 1
+        new_tmux_session "$session_name"
     fi
 }
 
